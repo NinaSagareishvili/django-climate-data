b4bb51e6aa6da1b1690ad3f264c540a9776d7d24
Nina Sagareishvili <ninasagareishvili@mac.speedport.ip>
2025-02-18 22:01:37 +0100

    Updated docker-compose.yml and fixed Django startup issues

---
+++ .env.sh
@@ -0,0 +1,12 @@
+# Django Settings
+DJANGO_SECRET_KEY=your_secret_key_here
+DEBUG=True
+DJANGO_ALLOWED_HOSTS=localhost,127.0.0.1
+
+# Datenbank Einstellungen
+DATABASE_ENGINE=django.db.backends.postgresql
+DATABASE_NAME=climate_db
+DATABASE_USERNAME=climate_user
+DATABASE_PASSWORD=securepassword
+DATABASE_HOST=db
+DATABASE_PORT=5432
